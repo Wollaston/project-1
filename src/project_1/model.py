118bb152ec2765bf6c763e4776665b67afcc0dcf
Wollaston <matt@wollaston.dev>
2025-10-05 23:57:01 -0400

    Fix cnn based on cnn for nlp ref instead of computer vision

--- src/project_1/model.py
+++ src/project_1/model.py
@@ -7,9 +7,10 @@ The only implementation for this module is implementing multinomial logistic reg
 using the subclass of ``torch.nn.Module``.
 """
 
-import torch.nn.functional as F
-import torch.nn as nn
+import numpy as np
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 
 # via Project 0
@@ -50,25 +51,42 @@ class MLP(nn.Module):
 
 
 # Basic CNN ref: https://docs.pytorch.org/tutorials/beginner/blitz/neural_networks_tutorial.html
+# CNN NLP ref: https://colab.research.google.com/drive/1b7aZamr065WPuLpq9C4RU6irB59gbX_K#scrollTo=ejGLw8TKViBY
 class CNN(nn.Module):
     """Convolutional Neural Net Model"""
 
-    def __init__(self, input_size: int, output_size: int) -> None:
+    def __init__(
+        self,
+        input_size: int,
+        output_size: int,
+        filter_sizes=[1, 1, 1],
+        num_filters=[100, 100, 100],
+    ) -> None:
         super().__init__()
-        self.conv1 = nn.Conv1d(input_size, 6, 5)
-        self.pool = nn.MaxPool1d(2, 2)
-        self.conv2 = nn.Conv1d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 256)
-        self.fc2 = nn.Linear(256, 128)
-        self.fc3 = nn.Linear(128, output_size)
+        # Conv Network
+        self.conv1d_list = nn.ModuleList(
+            [
+                nn.Conv1d(
+                    in_channels=input_size,
+                    out_channels=num_filters[i],
+                    kernel_size=filter_sizes[i],
+                )
+                for i in range(len(filter_sizes))
+            ]
+        )
+        # Fully-connected layer and Dropout
+        self.fc = nn.Linear(np.sum(num_filters), output_size)
 
     def forward(self, input):
-        c1 = F.relu(self.conv1(input))
-        s2 = F.max_pool1d(c1, (2, 2))
-        c3 = F.relu(self.conv2(s2))
-        s4 = F.max_pool1d(c3, 2)
-        s4 = torch.flatten(s4, 1)
-        f5 = F.relu(self.fc1(s4))
-        f6 = F.relu(self.fc2(f5))
-        output = self.fc3(f6)
-        return output
+        # Tensors come in as shape (batch_size, embed_size)
+        # We add a dummy dimension and permute the shape
+        # To match the expected input size for conv1d
+        input = torch.unsqueeze(input, 1)
+        input = input.permute(0, 2, 1)
+        x_conv_list = [F.relu(conv1d(input)) for conv1d in self.conv1d_list]
+        x_pool_list = [
+            F.max_pool1d(x_conv, kernel_size=x_conv.shape[2]) for x_conv in x_conv_list
+        ]
+        x_fc = torch.cat([x_pool.squeeze(dim=2) for x_pool in x_pool_list], dim=1)
+        logits = self.fc(x_fc)
+        return logits
